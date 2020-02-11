107e96748e7fa24f20333489ae68d366a49e2c32
Frank Dahncke <MechWarrior@gmx.com>
2020-02-11 08:58:51 +0100

    Projektdateien hinzufügen.

---
+++ frmMain.h
@@ -0,0 +1,66 @@
+#pragma once
+
+namespace CalculatorCLR {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Zusammenfassung für frmMain
+	/// </summary>
+	public ref class frmMain : public System::Windows::Forms::Form
+	{
+	public:
+		frmMain(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Konstruktorcode hier hinzufügen.
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		~frmMain()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// frmMain
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Name = L"frmMain";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
+			this->Text = L"frmMain";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
